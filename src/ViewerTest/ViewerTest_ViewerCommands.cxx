2579c831affd332d3188f47ef5a31e41429c89ac
Kirill Gavrilov <kirill@sview.ru>
2024-01-17 19:51:47 +0300

    Visualization - move Xlib implementation of Aspect_DisplayConnection to Xw_DisplayConnection

--- src/ViewerTest/ViewerTest_ViewerCommands.cxx
+++ src/ViewerTest/ViewerTest_ViewerCommands.cxx
@@ -95,6 +95,7 @@
   #include <WNT_WClass.hxx>
   #include <WNT_Window.hxx>
 #elif defined(HAVE_XLIB)
+  #include <Xw_DisplayConnection.hxx>
   #include <Xw_Window.hxx>
   #include <X11/Xlib.h>
   #include <X11/Xutil.h>
@@ -519,7 +520,7 @@ TCollection_AsciiString ViewerTest::ViewerInit (const ViewerTest_VinitParams& th
   #if defined(HAVE_XLIB)
     if (!theParams.DisplayName.IsEmpty())
     {
-      SetDisplayConnection (new Aspect_DisplayConnection (theParams.DisplayName));
+      SetDisplayConnection (new Xw_DisplayConnection (theParams.DisplayName));
     }
     else
     {
@@ -530,7 +531,7 @@ TCollection_AsciiString ViewerTest::ViewerInit (const ViewerTest_VinitParams& th
       Tcl_Interp* aTclInterp = aCommands.Interp();
       Tk_Window aMainWindow = Tk_MainWindow (aTclInterp);
       aDispX = aMainWindow != NULL ? Tk_Display (aMainWindow) : NULL;*/
-      SetDisplayConnection (new Aspect_DisplayConnection (aDispX));
+      SetDisplayConnection (new Xw_DisplayConnection (aDispX));
     }
   #else
     SetDisplayConnection (new Aspect_DisplayConnection ());
@@ -701,7 +702,7 @@ TCollection_AsciiString ViewerTest::ViewerInit (const ViewerTest_VinitParams& th
                                      Quantity_NOC_BLACK);
     VT_GetWindow()->RegisterRawInputDevices (WNT_Window::RawInputMask_SpaceMouse);
   #elif defined(HAVE_XLIB)
-    VT_GetWindow() = new Xw_Window (aGraphicDriver->GetDisplayConnection(),
+    VT_GetWindow() = new Xw_Window (Handle(Xw_DisplayConnection)::DownCast(aGraphicDriver->GetDisplayConnection()),
                                     aTitle.ToCString(),
                                     (int )aPxTopLeft.x(), (int )aPxTopLeft.y(),
                                     (int )aPxSize.x(), (int )aPxSize.y());
