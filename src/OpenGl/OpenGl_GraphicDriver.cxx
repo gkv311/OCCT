bbff0fbff12320d71e4f3ad26bdb7f6f4420ae84
Kirill Gavrilov <kirill@sview.ru>
2024-01-17 21:06:35 +0300

    Visualization - add Wayland_DisplayConnection and Wayland_Window
    CMake - added new option USE_WAYLAND for building OCCT with Wayland support.
    This option implicitly switches from GLX to EGL for building TKOpenGl.
    When both BUILD_RESOURCES and USE_WAYLAND options are enabled,
    extension protocol source files will be regenerated using wayland-scanner.
    
    Wayland_DisplayConnection - new class implementing
    Aspect_DisplayConnection interface for connecting to Wayland server.
    
    Wayland_Window - new class create Wayland window for testing 3D Viewer.
    As Wayland provides very low-level API suitable for GUI frameworks
    and not for end-user applications, the test window has many limitations:
    - no window decorations.
    
    External GUI framework should be preferred way to setup Wayland with OCCT 3D Viewer.
    
    vinit command in Draw Harness will create X11
    display connection by default, as it is more functional.
    vinit -wayland could be used for creating a test Wayland window.

--- src/OpenGl/OpenGl_GraphicDriver.cxx
+++ src/OpenGl/OpenGl_GraphicDriver.cxx
@@ -323,7 +323,7 @@ Standard_Boolean OpenGl_GraphicDriver::InitContext()
   EGLNativeDisplayType aDisplay = !myDisplayConnection.IsNull()
                                 ? (EGLNativeDisplayType)myDisplayConnection->GetDisplayAspect()
                                 : EGL_DEFAULT_DISPLAY;
-#if defined(HAVE_XLIB)
+#if defined(HAVE_XLIB) || defined(HAVE_WAYLAND)
   if (aDisplay == EGL_DEFAULT_DISPLAY)
   {
     ::Message::SendFail("Error: no connection to X11 display");
@@ -445,7 +445,7 @@ Standard_Boolean OpenGl_GraphicDriver::InitEglContext (Aspect_Display          t
 {
   ReleaseContext();
 #if defined(HAVE_EGL)
-#if defined(HAVE_XLIB)
+#if defined(HAVE_XLIB) || defined(HAVE_WAYLAND)
   if (myDisplayConnection.IsNull())
   {
     return Standard_False;
