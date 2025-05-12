881e0185e175016905dd68191b85d1c498bcce63
Kirill Gavrilov <kirill@sview.ru>
2025-05-12 13:06:57 +0300

    Coding, BRepTools - suppress MSVC warning C4005 strcasecmp macro redefinition '

--- src/BRepTools/BRepTools_ShapeSet.cxx
+++ src/BRepTools/BRepTools_ShapeSet.cxx
@@ -45,14 +45,6 @@
 #include <TopoDS_Shape.hxx>
 #include <TopoDS_Vertex.hxx>
 
-#ifdef MacOS
-#define strcasecmp(p,q) strcmp(p,q)
-#elif defined(_WIN32)
-#define strcasecmp strcmp
-#elif defined(AIX)
-#include <string.h>
-#endif
-
 // Modified:    02 Nov 2000: BUC60769. JMB, PTV.  In order to be able to read BRep 
 //              files that came from a platform different from where CasCade 
 //              is run, we need the following modifications.
