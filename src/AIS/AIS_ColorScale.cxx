123c2581c75015a7a9686684c7fef6fc5f8fc733
Pasukhin Dmitry <dpasukhi@opencascade.com>
2026-04-22 07:54:41 +0300

    Visualization - fix AIS_ColorScale rectangle triangle winding
    Add two triangles using consistent CCW orientation
    to avoid possible visual artifacts (visible seam egde or face culling).
    
    Script to reproduce:
    
    ```
    pload VISUALIZATION
    dpiaware 0
    vinit View1 -width 490 -height 409
    vcolorscale cs -demo
    vaspects cs -faceCulling BACK
    if { [vreadpixel 20 25 -name -rgb] != "RED" } {
      puts "Error: front face culled"
    }
    ```

--- src/AIS/AIS_ColorScale.cxx
+++ src/AIS/AIS_ColorScale.cxx
@@ -46,12 +46,11 @@ namespace
                               const Quantity_Color& theColorTop)
   {
     const Standard_Integer aVertIndex = theTris->VertexNumber() + 1;
+    theTris->AddVertex (gp_Pnt (theXLeft,            theYBottom + theSizeY, 0.0), theColorTop);
     theTris->AddVertex (gp_Pnt (theXLeft,            theYBottom,            0.0), theColorBottom);
     theTris->AddVertex (gp_Pnt (theXLeft + theSizeX, theYBottom,            0.0), theColorBottom);
-    theTris->AddVertex (gp_Pnt (theXLeft,            theYBottom + theSizeY, 0.0), theColorTop);
     theTris->AddVertex (gp_Pnt (theXLeft + theSizeX, theYBottom + theSizeY, 0.0), theColorTop);
-    theTris->AddEdges (aVertIndex,     aVertIndex + 1, aVertIndex + 2);
-    theTris->AddEdges (aVertIndex + 1, aVertIndex + 2, aVertIndex + 3);
+    theTris->AddQuadTriangleEdges (aVertIndex, aVertIndex + 1, aVertIndex + 2, aVertIndex + 3);
   }
 
   //! Compute hue angle from specified value.
