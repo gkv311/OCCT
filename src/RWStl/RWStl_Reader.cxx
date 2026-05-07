3c25b46784745a95a2f44b1ac3af8f0f1ad528b1
Kirill Gavrilov <kirill@sview.ru>
2026-05-07 07:09:58 +0300

    Coding, RWStl_Reader - remove dead code leftovers after patch for 0029399
    0029399 replaced sscanf() usage by Strtod() implementation
    parsing float values always in C locale.

--- src/RWStl/RWStl_Reader.cxx
+++ src/RWStl/RWStl_Reader.cxx
@@ -24,7 +24,6 @@
 #include <OSD_FileSystem.hxx>
 #include <OSD_Timer.hxx>
 #include <Poly_MergeNodesTool.hxx>
-#include <Standard_CLocaleSentry.hxx>
 
 #include <algorithm>
 #include <limits>
@@ -238,23 +237,6 @@ Standard_Boolean RWStl_Reader::IsAscii (Standard_IStream& theStream,
   return true;
 }
 
-// adapted from Standard_CString.cxx
-#ifdef __APPLE__
-  // There are a lot of *_l functions available on Mac OS X - we use them
-  #define SAVE_TL()
-#elif defined(_MSC_VER)
-  // MSVCRT has equivalents with slightly different syntax
-  #define SAVE_TL()
-  #define sscanf_l(theBuffer, theLocale, theFormat, ...) _sscanf_s_l(theBuffer, theFormat, theLocale, __VA_ARGS__)
-#else
-  // glibc provides only limited xlocale implementation:
-  // strtod_l/strtol_l/strtoll_l functions with explicitly specified locale
-  // and newlocale/uselocale/freelocale to switch locale within current thread only.
-  // So we switch to C locale temporarily
-  #define SAVE_TL() Standard_CLocaleSentry aLocaleSentry;
-  #define sscanf_l(theBuffer, theLocale, theFormat, ...) sscanf(theBuffer, theFormat, __VA_ARGS__)
-#endif
-
 // Macro to get 64-bit position of the file from std::streampos
 #if defined(_MSC_VER) && _MSC_VER < 1700
   // In MSVC 2010, cast of std::streampos to 64-bit int is implemented incorrectly;
@@ -336,10 +318,6 @@ Standard_Boolean RWStl_Reader::readAscii (TCollection_AsciiString& theName,
   MergeNodeTool aMergeTool (this);
   aMergeTool.SetMergeAngle (myMergeAngle);
   aMergeTool.SetMergeTolerance (myMergeTolearance);
-
-  Standard_CLocaleSentry::clocale_t aLocale = Standard_CLocaleSentry::GetCLocale();
-  (void)aLocale; // to avoid warning on GCC where it is actually not used
-  SAVE_TL() // for GCC only, set C locale globally
 
   // report progress every 1 MiB of read data
   const int aStepB = 1024 * 1024;
