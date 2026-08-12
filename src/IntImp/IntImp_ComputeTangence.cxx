f7bdb6430b13f99683e61e6ef2e8979f4cfc74cd
Kirill Gavrilov <kirill@sview.ru>
2026-08-12 17:57:12 +0300

    Coding, IntImp_Int2S - suppress GCC warning -Wmaybe-uninitialized on DuvBuf
    Also applied cosmetics to IntImp_ComputeTangence.

--- src/IntImp/IntImp_ComputeTangence.cxx
+++ src/IntImp/IntImp_ComputeTangence.cxx
@@ -13,176 +13,85 @@
 // commercial license or contractual agreement.
 
 #include <IntImp_ComputeTangence.hxx>
-#include <IntImp_ConstIsoparametric.hxx>
-#include <Standard_OutOfRange.hxx>
 
-static const IntImp_ConstIsoparametric staticChoixRef [4] = {
-IntImp_UIsoparametricOnCaro1,
-IntImp_VIsoparametricOnCaro1,
-IntImp_UIsoparametricOnCaro2,
-IntImp_VIsoparametricOnCaro2,
-};
-
-IntImp_ConstIsoparametric ChoixRef (Standard_Integer theIndex)
-{
-  Standard_OutOfRange_Raise_if (theIndex < 0 || theIndex > 3, "ChoixRef() in " __FILE__)
-  return staticChoixRef[theIndex];
-}
+#include <algorithm>
 
 //=======================================================================
 //function : IntImp_ComputeTangence
-//purpose  : 
+//purpose  :
 //=======================================================================
-Standard_Boolean IntImp_ComputeTangence(const gp_Vec DPuv[],
-					const Standard_Real EpsUV[],
-					Standard_Real Tgduv[],
-					IntImp_ConstIsoparametric TabIso[]) 
-//                 arguments d entree:
-// DPuv [0] =derivee en u sur caro 1    
-// DPuv [1] =derivee en v sur caro 1
-// DPuv [2] =derivee en u sur caro 2
-// DPuv [3] =derivee en v sur caro 2
-// EpsUV[0] tolerance en u sur caro1
-// EpsUV[1] tolerance en v sur caro1
-// EpsUV[2] tolerance en u sur caro2
-// EpsUV[3] tolerance en v sur caro2
-//                 arguments de sortie:
-// Tgduv[0] composante sup dp/du de caro1 de la tangente a l intersection
-// Tgduv[1] composante sup dp/dv de caro1 de la tangente a l intersection
-// Tgduv[2] composante sup dp/du de caro2 de la tangente a l intersection
-// Tgduv[3] composante sup dp/dv de caro2 de la tangente a l intersection
-// TabIso[0...3] meilleure iso range par ordre decroissant candidate 
-//               a l intersection
-//         algorithme
-// calculer la tangente a l 'intersection ;en utilisant la propriete suivante
-// du produit scalaire a^(b^c)=b(ac)-c(ab) on obtient les composantes de la
-// tangente a l intersection dans les 2 plans tangents (t=n1^n2 ou n1 normale
-// au premier carreau n2 au 2ieme)
-// on s assurera que les plans tangents des 2 carreaux ne sont pas //
-// les composantes de l intersection dans les plans tangents permettent de 
-//determiner l  angle entre les isoparametriques d un carreau avec le carreau
-//reciproque 
-//on triera par ordre croissant les cosinus :le plus petit cosinus determine le
-// meilleure angle donc la meilleure iso a choisir pour trouver
-// l intersection 
-
+Standard_Boolean IntImp_ComputeTangence(const gp_Vec (&theDPuv)[4],
+                                        const Standard_Real (&theEpsUV)[4],
+                                        Standard_Real (&theTgduv)[4],
+                                        IntImp_ConstIsoparametric (&theTabIso)[4])
 {
-   Standard_Real NormDuv[4], aM2, aTol2;
-   Standard_Integer i;
-   //
-   aTol2=1.e-32;
-   //
-   for (i=0; i<4; ++i) {
-     NormDuv[i] = DPuv[i].SquareMagnitude();    
-     if(NormDuv[i]<=aTol2) { 
-       return Standard_True;
-     } 
-   }
-   //
-   //-------------------------------------------------
-   gp_Vec N1 = DPuv[0];
-   N1.Cross(DPuv[1]);
-   //
-   //modified by NIZNHY-PKV Tue Nov 01 08:37:32 2011f
-   aM2=N1.SquareMagnitude();
-   if (aM2<aTol2) {
-     return Standard_True;
-   }
-   //modified by NIZNHY-PKV Tue Nov 01 08:37:34 2011t
-   N1.Normalize();
-   //-------------------------------------------------
-   gp_Vec N2 = DPuv[2];
-   N2.Cross(DPuv[3]);
-   //modified by NIZNHY-PKV Tue Nov 01 08:37:32 2011f
-   aM2=N2.SquareMagnitude();
-   if (aM2<aTol2) {
-     return Standard_True;
-   }
-   //modified by NIZNHY-PKV Tue Nov 01 08:37:34 2011t
-   N2.Normalize();
-   //
-   //modified by NIZNHY-PKV Tue Nov 01 08:31:25 2011f
-   for (i=0; i<4; ++i) {
-     NormDuv[i]=sqrt(NormDuv[i]);
-   }  
-   //modified by NIZNHY-PKV Tue Nov 01 08:31:29 2011t
-   Tgduv[0] = -DPuv[1].Dot(N2);
-   Tgduv[1] =  DPuv[0].Dot(N2);
-   Tgduv[2] =  DPuv[3].Dot(N1); 
-   Tgduv[3] = -DPuv[2].Dot(N1);
+  constexpr Standard_Real aTol2 = 1.e-32;
+  Standard_Real aNormDuv[4];
+  for (Standard_Integer i = 0; i < 4; ++i)
+  {
+    aNormDuv[i] = theDPuv[i].SquareMagnitude();
+    if (aNormDuv[i] <= aTol2)
+      return true;
+  }
 
+  gp_Vec aN1 = theDPuv[0] ^ theDPuv[1];
+  const Standard_Real aM1 = aN1.SquareMagnitude();
+  if (aM1 < aTol2)
+    return true;
 
-   Standard_Boolean tangent = (Abs(Tgduv[0]) <= EpsUV[0]*NormDuv[1] &&
-			       Abs(Tgduv[1]) <= EpsUV[1]*NormDuv[0] &&
-			       Abs(Tgduv[2]) <= EpsUV[2]*NormDuv[3] &&
-			       Abs(Tgduv[3]) <= EpsUV[3]*NormDuv[2]  );
-   if(!tangent) { 
-     Standard_Real t=N1.Dot(N2);
-     if(t<0.0) t=-t;
-     if(t>0.999999999) { 
-       tangent=Standard_True; 
-     }
-   }
+  gp_Vec aN2 = theDPuv[2] ^ theDPuv[3];
+  const Standard_Real aM2 = aN2.SquareMagnitude();
+  if (aM2 < aTol2)
+    return true;
 
-   if (!tangent) {
-     NormDuv[0] = Abs(Tgduv[1]) /NormDuv[0]; //iso u sur caro1
-     NormDuv[1] = Abs(Tgduv[0]) /NormDuv[1]; //iso v sur caro1
-     NormDuv[2] = Abs(Tgduv[3]) /NormDuv[2]; // iso u sur caro2
-     NormDuv[3] = Abs(Tgduv[2]) /NormDuv[3]; //iso v sur caro2
+  aN1 /= Sqrt(aM1);
+  aN2 /= Sqrt(aM2);
+  for (Standard_Integer i = 0; i < 4; ++i)
+    aNormDuv[i] = Sqrt(aNormDuv[i]);
 
+  theTgduv[0] = -theDPuv[1].Dot(aN2);
+  theTgduv[1] =  theDPuv[0].Dot(aN2);
+  theTgduv[2] =  theDPuv[3].Dot(aN1);
+  theTgduv[3] = -theDPuv[2].Dot(aN1);
+  const bool isTangent = (Abs(theTgduv[0]) <= theEpsUV[0] * aNormDuv[1]
+                       && Abs(theTgduv[1]) <= theEpsUV[1] * aNormDuv[0]
+                       && Abs(theTgduv[2]) <= theEpsUV[2] * aNormDuv[3]
+                       && Abs(theTgduv[3]) <= theEpsUV[3] * aNormDuv[2]);
+  if (isTangent)
+    return true;
 
+  constexpr Standard_Real aTangDot = 0.999999999;
+  const Standard_Real aCosAng = Abs(aN1.Dot(aN2));
+  if (aCosAng > aTangDot)
+    return true;
 
-     //-- Tri sur NormDuv  ( en para. avec ChoixRef ) 
-     Standard_Boolean triOk = Standard_False;
-     Standard_Real t;
-     IntImp_ConstIsoparametric ti;
-     for ( i=0;i<=3;i++)
-     {
-       TabIso[i] = staticChoixRef[i];
-     }
-     do { 
-       triOk = Standard_True;
-       for(i=1;i<=3;i++) { 
-	 if(NormDuv[i-1]>NormDuv[i]) { 
-	   triOk=Standard_False;
-	   t=NormDuv[i]; 
-	   NormDuv[i]=NormDuv[i-1];
-	   NormDuv[i-1]=t;
-	   
-	   ti = TabIso[i];
-	   TabIso[i] = TabIso[i-1];
-	   TabIso[i-1] = ti;
-	 }
-       }
-     }
-     while(!triOk);
+  Standard_Real aNormTgduv[4] =
+  {
+    Abs(theTgduv[1]) / aNormDuv[0], // iso u along surface 1
+    Abs(theTgduv[0]) / aNormDuv[1], // iso v along surface 1
+    Abs(theTgduv[3]) / aNormDuv[2], // iso u along surface 2
+    Abs(theTgduv[2]) / aNormDuv[3]  // iso v along surface 2
+  };
 
+  // Sort aNormTgduv (in parallel with theTabIso)
+  theTabIso[0] = IntImp_UIsoparametricOnCaro1;
+  theTabIso[1] = IntImp_VIsoparametricOnCaro1;
+  theTabIso[2] = IntImp_UIsoparametricOnCaro2;
+  theTabIso[3] = IntImp_VIsoparametricOnCaro2;
+  bool isSorted = false;
+  do
+  {
+    isSorted = true;
+    for (Standard_Integer i = 1; i < 4; ++i)
+    {
+      if (aNormTgduv[i - 1] > aNormTgduv[i])
+      {
+        isSorted = false;
+        std::swap(aNormTgduv[i], aNormTgduv[i - 1]);
+        std::swap(theTabIso[i], theTabIso[i - 1]);
+      }
+    }
+  } while (!isSorted);
 
-#if 0     
-     // trier par ordre croissant le tableau NormDuv
-     Standard_Integer II;
-     for (j =0;j<=3;j++) Irang[j]=j;
-     for (j =0;j<=3;j++) {
-       Tampon = NormDuv[j];
-       II=j;
-       for (i =j+1;i<=3;i++) {
-         if (NormDuv[i] < Tampon) {
-	   Tampon = NormDuv[i];
-	   II = i;
-         }
-       }
-       Irang[j] = Irang[II];
-       Irang[II] = j;
-       NormDuv[II] = NormDuv[j];
-       NormDuv[j] = Tampon;
-     }
-     for (j=0; j<=3;j++)
-     {
-       TabIso[j] = staticChoixRef[Irang[j]];     
-     }
-#endif
-   }
-   return tangent;
- }
-
-
+  return false;
+}
